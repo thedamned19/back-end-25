ebc5f504da0b24c64d95d9e974e8212f95c8b6af
Ernesto Leimsieder <ernesto.leimsieder@imm.gub.uy>
2025-04-18 13:15:38 -0300

    Clase1

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 const Contador = require("./contador");
 
 const contador1 = new Contador("Nahuel Navarro");
+const contador2 = new Contador("Jorge Navarro");
 
 contador1.contar();
+
 console.log(contador1.getResponsable());
 console.log(contador1.getCuentaIndividual());
 console.log(contador1.getCuentaGlobal());
+
+contador2.contar();
+
+console.log(contador2.getResponsable());
+console.log(contador2.getCuentaIndividual());
+console.log(contador2.getCuentaGlobal());
