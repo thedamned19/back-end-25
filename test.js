c4a51c897945568f99fa88f1ab36fdfe35bc5860
Ernesto Leimsieder <ernesto.leimsieder@imm.gub.uy>
2025-04-22 19:07:29 -0300

    Clase1

--- test.js
+++ test.js
@@ -2,15 +2,34 @@ const Contador = require("./contador");
 
 const contador1 = new Contador("Nahuel Navarro");
 const contador2 = new Contador("Jorge Navarro");
+const contador3 = new Contador("Brunno Navarro");
 
 contador1.contar();
 
-console.log(contador1.getResponsable());
-console.log(contador1.getCuentaIndividual());
-console.log(contador1.getCuentaGlobal());
+let resp = `El responsable es ${contador1.getResponsable()}`;
+let cuentaI = `Cuenta individual ${contador1.getCuentaIndividual()}`;
+let cuentaG = `Cuenta global ${contador1.getCuentaGlobal()}`;
+
+console.log(resp);
+console.log(cuentaI);
+console.log(cuentaG);
 
 contador2.contar();
 
-console.log(contador2.getResponsable());
-console.log(contador2.getCuentaIndividual());
-console.log(contador2.getCuentaGlobal());
+resp = `El responsable es ${contador2.getResponsable()}`;
+cuentaI = `Cuenta individual ${contador2.getCuentaIndividual()}`;
+cuentaG = `Cuenta global ${contador2.getCuentaGlobal()}`;
+
+console.log(resp);
+console.log(cuentaI);
+console.log(cuentaG);
+
+contador3.contar();
+
+resp = `El responsable es ${contador3.getResponsable()}`;
+cuentaI = `Cuenta individual ${contador3.getCuentaIndividual()}`;
+cuentaG = `Cuenta global ${contador3.getCuentaGlobal()}`;
+
+console.log(resp);
+console.log(cuentaI);
+console.log(cuentaG);
